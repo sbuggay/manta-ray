081d9f2adeed12fa781489e84bc0a30710f00b21
Devan Buggay <debuggay@microsoft.com>
2018-03-07 15:57:25 -0800

    Add documentation stubs

--- src/main.c
+++ src/main.c
@@ -1,17 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 #include "ray.h"
 
+/**
+ * @brief 
+ * 
+ * @param center 
+ * @param radius 
+ * @param r 
+ * @return int 
+ */
 int hit_sphere(vec3 center, float radius, ray r) {
     vec3 oc = vec3_sub(r.a, center);
     float a = vec3_dot(r.b, r.b);
     float b = 2.0 * vec3_dot(oc, r.b);
     float c = vec3_dot(oc, oc) - radius * radius;
     float discriminant = b * b - 4 * a * c;
-    return (discriminant > 0);
+    if (discriminant < 0) {
+        return -1.0;
+    }
+    else {
+        return (-1.0 * b - sqrt(discriminant)) / (2.0 * a);
+    }
 }
 
+/**
+ * @brief 
+ * 
+ * @param r 
+ * @return vec3 
+ */
 vec3 color(ray r) {
     if (hit_sphere(vec3_create(0, 0, -1), 0.5, r)) {
         return vec3_create(1, 0, 0);
@@ -24,16 +44,13 @@ vec3 color(ray r) {
 }
 
 int main(int argc, char *argv[]) {
-
-    vec3 a = vec3_create(5, 5, 5);
-    vec3 b = vec3_create(2, 2, 1);
-    vec3 c = vec3_add(a, b);
-
+    // Viewport vectors
     vec3 lower_left_corner = vec3_create(-2.0, -1.0, -1.0);
     vec3 horizontal = vec3_create(4.0, 0.0, 0.0);
     vec3 vertical = vec3_create(0.0, 2.0, 0.0);
     vec3 origin = vec3_create(0.0, 0.0, 0.0);
 
+    // Viewport dimensions
     int nx = 200;
     int ny = 100;
     int i, j;
--- src/vec3.h
+++ src/vec3.h
@@ -1,22 +1,65 @@
 #ifndef VEC3H
 #define VEC3H
 
+/**
+ * @brief 
+ * 
+ */
 typedef struct {
     double x;
     double y;
     double z;
 } vec3;
 
+/**
+ * @brief 
+ * 
+ * @param x 
+ * @param y 
+ * @param z 
+ * @return vec3 
+ */
 vec3 vec3_create(double x, double y, double z);
+
+/**
+ * @brief 
+ * 
+ * @param a 
+ * @param b 
+ * @return vec3 
+ */
 vec3 vec3_add(vec3 a, vec3 b);
+
+/**
+ * @brief 
+ * 
+ * @param a 
+ * @param b 
+ * @return vec3 
+ */
 vec3 vec3_sub(vec3 a, vec3 b);
+
+/**
+ * @brief 
+ * 
+ * @param a 
+ * @param b 
+ * @return vec3 
+ */
 vec3 vec3_mul(vec3 a, vec3 b);
+
 vec3 vec3_div(vec3 a, vec3 b);
+
 vec3 vec3_mul_scalar(vec3 a, double t);
+
 vec3 vec3_div_scalar(vec3 a, double t);
+
 float vec3_dot(vec3 a, vec3 b);
+
 vec3 vec3_cross(vec3 a, vec3 b);
+
 double vec3_length(vec3 a);
+
 vec3 vec3_unit(vec3 a);
 
 #endif
